c0954cf4fdd8954452b2659b05d269cd7ae5c5f5
Fabiano César <fcfabiano.cesar@icloud.com>
2021-03-13 16:46:40 -0300

    Puxando repositórios do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,29 @@
-const repositoryName = 'unform';
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+// https://api.github.com/users/fcrfabiano
 
 export function RepositoryList() {
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/fcrfabiano/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryList
+                {
+                    repositories.map(repository => {
+                        return <RepositoryItem repository={repository} />
+                    })
+                }
             </ul>
 
         </section>
